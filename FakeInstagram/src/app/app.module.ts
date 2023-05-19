8020cf73aa43dab22d12dbe75e9b36a5e18ef8a6
Juan-Tabarez <jtabarez6@gmail.com>
2023-05-19 16:03:40 -0300

    images

--- FakeInstagram/src/app/app.module.ts
+++ FakeInstagram/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { CarreteComponent } from './carrete/carrete.component';
+import { CarreteComponent } from './photo-roll/photo-roll.component';
 import { ImagesComponent } from './images/images.component';
 import { UnselectedButtonComponent } from './unselected-button/unselected-button.component';
 import { SelectedButtonComponent } from './selected-button/selected-button.component';
